d3d446b2916c3b0ee4b5205e82ce5da28cf86e1e
Fredrik Anthonisen <fredrik.anthonisen94@gmail.com>
2016-12-14 13:20:24 +0100

    Added the updated version of decoding script, added more predeployment frames from 13.12.16

--- PSSEP/python_scripts/ttn_data_ascii_conversion.py
+++ PSSEP/python_scripts/ttn_data_ascii_conversion.py
@@ -10,6 +10,7 @@ def litte_to_big_endian(hex_number):
             temp = hex_number[x-1: x+1] + temp
     return temp
 
+
 def battery_conversion(hex_letter):
     if hex_letter == 'A':
         return 10
@@ -28,32 +29,52 @@ def battery_conversion(hex_letter):
 
 for line in stdin:
     temp = line[36:]
+    #create a list to save measurement values
     measurements = []
+    #append co2 value (hex)
     measurements.append(temp[2:10])
+    #append no2 value (hex)
     measurements.append(temp[12:20])
+    #append temp value (hex)
     measurements.append(temp[22:30])
+    #append humidity value (hex)
     measurements.append(temp[32:40])
+    #append pressure value (hex)
     measurements.append(temp[42:50])
+    #if node has PMx then
     if(len(temp) > 55):
+        #append PM1 (hex)
         measurements.append(temp[52:60])
+        #append PM2.5 (hex)
         measurements.append(temp[62:70])
+        #append PM10 (hex)
         measurements.append(temp[72:80])
+        #append battery (hex)
         measurements.append(temp[82:84])
+    #if node does not have PMx
     else:
+        #append battery value (hex)
         measurements.append(temp[52:54])
-    
+
+    #if node has PMX sensor, convert all values but battery level from little to big endian
     if len(measurements) == 9:
         for x in range(0, 8):
             measurements[x] = litte_to_big_endian(measurements[x])
+            #use struct.unpack to convert from hex to decimal
             measurements[x] = struct.unpack('!f', measurements[x].decode('hex'))[0]
+        #convert the battery level from hex to decimal (uses it's own function since it's only 2 digits)
         measurements[8] = (16*battery_conversion(measurements[8][0])) + battery_conversion(measurements[8][1])
+    #if node doesn't have PMX, convert all vaues but battery level from little to big endian
     else:
         for x in range(0, 5):
             measurements[x] = litte_to_big_endian(measurements[x])
+            #use struct.unpack to convert from hex to decimal
             measurements[x] = struct.unpack('!f', measurements[x].decode('hex'))[0]
+        #convert battery from 2 digit hex to decimal
         measurements[5] = (16*battery_conversion(measurements[5][0])) + battery_conversion(measurements[5][1])
-    
-    with open('output_10.txt', 'a') as data:
+
+    #write data to file
+    with open('node.txt', 'a') as data:
         data.writelines('%s ' % item for item in measurements)
         data.write('\n')
         data.close()
